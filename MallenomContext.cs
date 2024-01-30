75086397bbec007dfde60332302682240ef5885d
MetaException <67053613+MetaException@users.noreply.github.com>
2024-01-30 18:52:34 +0300

    Add and scaffold database

---
+++ MallenomContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using stage3.Model;
+
+namespace stage3;
+
+public partial class MallenomContext : DbContext
+{
+    public MallenomContext()
+    {
+    }
+
+    public MallenomContext(DbContextOptions<MallenomContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=mallenom;Username=postgres;Password=4409");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("users");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityAlwaysColumn()
+                .HasColumnName("id");
+            entity.Property(e => e.LoginHash)
+                .HasMaxLength(255)
+                .HasColumnName("login_hash");
+            entity.Property(e => e.PassHash)
+                .HasMaxLength(255)
+                .HasColumnName("pass_hash");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stage3.Model;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string LoginHash { get; set; } = null!;
+
+    public string PassHash { get; set; } = null!;
+}
