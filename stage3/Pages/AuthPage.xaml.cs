e8994cdd983b042b3b6ed15cfe1fe8c04e7f810a
MetaException <67053613+MetaException@users.noreply.github.com>
2024-01-31 17:50:31 +0300

    [stage3] Add inforamtion label

--- stage3/Pages/AuthPage.xaml.cs
+++ stage3/Pages/AuthPage.xaml.cs
@@ -11,19 +11,50 @@ namespace stage3.Pages
             InitializeComponent();
         }
 
-        private void LoginBtn_Clicked(object sender, EventArgs e)
+        private async void LoginBtn_Clicked(object sender, EventArgs e)
         {
             LoginBtn.IsEnabled = false;
-            if (LoginEntry.Text != "" && PasswordEntry.Text != "")
-                _dbUtils.AuthorizeUser(LoginEntry.Text, PasswordEntry.Text);
+
+            try
+            {
+                bool isAuthorized = await _dbUtils.AuthorizeUser(LoginEntry.Text, PasswordEntry.Text);
+                if (isAuthorized)
+                {
+                    ErrorLabel.TextColor = Colors.Black;
+                    ErrorLabel.Text = "Вы успешно авторизовались";
+                }
+                else
+                {
+                    ErrorLabel.TextColor = Colors.Red;
+                    ErrorLabel.Text = "Неверный логин или пароль";
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.TextColor = Colors.Red;
+                ErrorLabel.Text = ex.Message;
+            }
             LoginBtn.IsEnabled = true;
         }
 
-        private void RegBtn_Clicked(object sender, EventArgs e)
+        private async void RegBtn_Clicked(object sender, EventArgs e)
         {
             RegBtn.IsEnabled = false;
-            if (LoginEntry.Text != "" && PasswordEntry.Text != "")
-                _dbUtils.RegisterNewUser(LoginEntry.Text, PasswordEntry.Text);
+
+            try
+            {
+                bool isRegistered = await _dbUtils.RegisterNewUser(LoginEntry.Text, PasswordEntry.Text);
+                if (isRegistered)
+                {
+                    ErrorLabel.TextColor = Colors.Black;
+                    ErrorLabel.Text = "Вы успешно зарегистрировались";
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLabel.TextColor = Colors.Red;
+                ErrorLabel.Text = ex.Message;
+            }
             RegBtn.IsEnabled = true;
         }
     }
--- stage3/Utils/DbUtils.cs
+++ stage3/Utils/DbUtils.cs
@@ -1,9 +1,11 @@
-﻿using stage3.Model;
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using stage3.Model;
 
 namespace stage3.Utils
 {
     public class DbUtils
     {
+        //Разобраться с async
         private readonly MallenomContext _context;
 
         public DbUtils(MallenomContext context)
@@ -13,10 +15,13 @@ namespace stage3.Utils
 
         public async Task<bool> AuthorizeUser(string login, string password)
         {
+            if (login is null || password is null)
+                return false;
+
             var user = await FindUserByLogin(login);
             if (user is not null)
             {
-                return BCrypt.Net.BCrypt.Verify(password, user.PassHash);
+                return await Task.Run(async () => BCrypt.Net.BCrypt.Verify(password, user.PassHash)); //Не поддерживает await ???
             }
             return false;
         }
@@ -25,7 +30,7 @@ namespace stage3.Utils
         {
             foreach (var user in _context.Users)
             {
-                if (BCrypt.Net.BCrypt.Verify(login, user.LoginHash))
+                if (await Task.Run(async () => BCrypt.Net.BCrypt.Verify(login, user.LoginHash)))
                     return user;
             }
             return null;
@@ -35,30 +40,37 @@ namespace stage3.Utils
         {
             foreach (var user in _context.Users)
             {
-                if (BCrypt.Net.BCrypt.Verify(login, user.LoginHash))
+                bool isExists = await Task.Run(async () => BCrypt.Net.BCrypt.Verify(login, user.LoginHash));
+                if (isExists)
                     return true;
             }
             return false;
         }
 
-        public async void RegisterNewUser(string login, string password)
+        public async Task<bool> RegisterNewUser(string login, string password) //потом вернуть на void
         {
+            if (login is null || password is null)
+                return false;
+
             if (!await CheckIfUserExists(login)) //убрать?
             {
                 User toReg = new User
                 {
-                    PassHash = HashPassword(password),
-                    LoginHash = HashPassword(login)
+                    PassHash = await HashPassword(password),
+                    LoginHash = await HashPassword(login)
                 };
 
                 await _context.Users.AddAsync(toReg);
                 await _context.SaveChangesAsync();
+
+                return true;
             }
+            return false;
         }
 
-        public static string HashPassword(string text)
+        public static async Task<string> HashPassword(string text)
         {
-            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(text);
+            string hashedPassword = await Task.Run(async () => BCrypt.Net.BCrypt.HashPassword(text));
 
             return hashedPassword;
         }
