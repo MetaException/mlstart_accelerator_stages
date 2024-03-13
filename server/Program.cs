73d88e9572d843426f7c205246293dc717e2a465
MetaException <67053613+MetaException@users.noreply.github.com>
2024-03-13 15:47:27 +0300

    Resturcture program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -15,6 +15,94 @@ namespace server
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            CreateLogger();
+
+            ConfigureBuilder(builder);
+            ConfigurateWithAppSettings(builder);
+
+            var app = builder.Build();
+
+            ConfigureApp(app);
+
+            _ = Simulator.SimulateLoop();
+
+            app.Run();
+        }
+
+        private static void ConfigureBuilder(WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog();
+
+            builder.Services.AddControllers();
+
+            builder.Configuration.AddJsonFile("appsettings.json");
+
+            builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+            builder.Services.AddDbContext<MallenomContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionUrl")));
+
+            //builder.Services.AddHealthChecks()
+            //    .AddDbContextCheck<MallenomContext>();
+
+            // https://learn.microsoft.com/ru-ru/aspnet/core/security/authorization/secure-data?view=aspnetcore-8.0
+            builder.Services.AddIdentity<IdentityUser<int>, IdentityRole<int>>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 1;
+            })
+                .AddDefaultTokenProviders()
+                .AddEntityFrameworkStores<MallenomContext>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("CCEA5D06F64497D9CCB548B70B024ASGESGHES5H50")),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        }
+
+        private static void ConfigureApp(WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                //app.UseExceptionHandler("/Home/Error"); у нас нет views
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseSerilogRequestLogging();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            //app.MapHealthChecks("/healtha");
+        }
+
+        private static void CreateLogger()
+        {
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Logger(l => l
                     .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Debug)
@@ -55,82 +143,16 @@ namespace server
                 .MinimumLevel.Verbose()
                 .WriteTo.Console()
                 .CreateLogger();
+        }
 
-            builder.Host.UseSerilog();
-
-            InitilizeApp(builder);
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-            app.UseSerilogRequestLogging();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
+        private static void ConfigurateWithAppSettings(WebApplicationBuilder builder)
+        {
             Simulator.tasksDelay = Convert.ToDouble(builder.Configuration.GetRequiredSection("SimulatorConfiguration")["TasksDelayInSeconds"]);
             SimulatorLogger.MaxLogsCount = Convert.ToInt32(builder.Configuration.GetRequiredSection("SimulatorConfiguration")["MaxLogsCount"]);
 
-            _ = Simulator.SimulateLoop();
-
             var configSection = builder.Configuration.GetSection("ConnectionConfiguration");
-            app.Run($"https://{configSection["ServerIp"]}:{configSection["ServerPort"]}");
-        }
 
-        private static void InitilizeApp(WebApplicationBuilder builder)
-        {
-            builder.Services.AddControllers();
-
-            builder.Configuration.AddJsonFile("appsettings.json");
-
-            builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
-            builder.Services.AddDbContext<MallenomContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionUrl")));
-
-            // https://learn.microsoft.com/ru-ru/aspnet/core/security/authorization/secure-data?view=aspnetcore-8.0
-            builder.Services.AddIdentity<IdentityUser<int>, IdentityRole<int>>(options =>
-            {
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequiredLength = 1;
-            })
-                .AddDefaultTokenProviders()
-                .AddEntityFrameworkStores<MallenomContext>();
-
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("CCEA5D06F64497D9CCB548B70B024ASGESGHES5H50")),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ValidateLifetime = true,
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
+            builder.WebHost.UseUrls($"https://{configSection["ServerIp"]}:{configSection["ServerPort"]}");
         }
     }
 }
