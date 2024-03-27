62cdd5d61eca1f1b33217a424e0a9eeb8f28ecd4
MetaException <67053613+MetaException@users.noreply.github.com>
2024-03-27 22:45:10 +0300

    Implement test ui

--- api_client/ViewModels/MainPageViewModel.cs
+++ api_client/ViewModels/MainPageViewModel.cs
@@ -1,26 +1,65 @@
 ﻿using apiclient.Utils;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace apiclient.ViewModels;
 
 public partial class MainPageViewModel : ObservableObject
 {
+    public class Item
+    {
+        public ImageSource ItemImageSource { get; set; }
+        public FileResult FilePath { get; set; }
+    }
+
     private readonly NetUtils _netUtils;
 
     [ObservableProperty]
     private bool _isInternetErrorVisible;
 
+    [ObservableProperty]
+    private ImageSource _imgSource;
+
+    [ObservableProperty]
+    private string _imageWidth;
+
+    [ObservableProperty]
+    private string _imageHeight;
+
+    [ObservableProperty]
+    private string _imageChannels;
+
+    [ObservableProperty]
+    private ObservableCollection<Item> _imgs = new ObservableCollection<Item>();
+
+    [ObservableProperty]
+    private Item _selectedItem;
+
+    [ObservableProperty]
+    private bool _isUploadButtonVisible = false;
+
     public RelayCommand UploadButtonClickedCommand { get; }
+
+    public RelayCommand OpenFileCommand { get; }
+
+    public RelayCommand<Item> SelectionChangedCommand { get; }
 
     public MainPageViewModel(NetUtils netUtils)
     {
         _netUtils = netUtils;
 
         UploadButtonClickedCommand = new RelayCommand(async () => await UploadButtonClicked());
+        OpenFileCommand = new RelayCommand(async () => await OpenFile());
+        SelectionChangedCommand = new RelayCommand<Item>(async (item) => await SelectionChangedHandler(item));
     }
 
-    private async Task UploadButtonClicked()
+    private async Task SelectionChangedHandler(Item item)
+    {
+        ImgSource = ImageSource.FromFile(item.FilePath.FullPath);
+    }
+
+    private async Task OpenFile()
     {
         var results = await FilePicker.PickMultipleAsync(new PickOptions
         {
@@ -28,10 +67,34 @@ public partial class MainPageViewModel : ObservableObject
             FileTypes = FilePickerFileType.Images
         });
 
-        foreach (var result in results)
+        if (!results.Any())
+            return;
+
+        ImgSource = ImageSource.FromFile(results.First().FullPath);
+
+        foreach (var file in results)
         {
-            var fileStream = await result.OpenReadAsync();
-            var details = await _netUtils.SendImageAsync(fileStream, result.FileName);
+            Imgs.Add(new Item { ItemImageSource = ImageSource.FromFile(file.FullPath), FilePath = file });
         }
+
+        SelectedItem = Imgs.Last();
+
+        IsUploadButtonVisible = true;
+    }
+
+    private async Task UploadButtonClicked()
+    {
+        if (SelectedItem is null)
+            return;
+
+        var result = SelectedItem.FilePath;
+
+        var fileStream = await result.OpenReadAsync();
+
+        var details = await _netUtils.SendImageAsync(fileStream, result.FileName);
+
+        ImageWidth = $"Ширина: {details.width}";
+        ImageHeight = $"Высота: {details.height}";
+        ImageChannels = $"Количество каналов: {details.channels}";
     }
 }
